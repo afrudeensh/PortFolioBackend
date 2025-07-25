9022ea8da7cefdc5c583c5668510c7e1339aaf57
Afrudeen S H <115522869+afrudeensh@users.noreply.github.com>
2025-07-25 06:22:39 +0530

    Initial Commit

--- src/main/java/com/sha/controller/ContactController.java
+++ src/main/java/com/sha/controller/ContactController.java
@@ -1,4 +1,38 @@
 package com.sha.controller;
 
+import com.sha.request.ContactRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/contact")
+@CrossOrigin(origins = "*")
 public class ContactController {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @PostMapping("/send")
+    public ResponseEntity<?> sendMessage(@Valid @RequestBody ContactRequest request) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo("afrudeenhasina15301@gmail.com");
+            message.setSubject("New Message from Portfolio");
+            message.setText(
+                    "Name: " + request.getName() + "\n" +
+                            "Email: " + request.getEmail() + "\n\n" +
+                            "Message:\n" + request.getMessage()
+            );
+
+            mailSender.send(message);
+
+            return ResponseEntity.ok("Message sent successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Message Failed to send");
+        }
+    }
 }
--- src/main/java/com/sha/request/ContactRequest.java
+++ src/main/java/com/sha/request/ContactRequest.java
@@ -1,4 +1,20 @@
 package com.sha.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ContactRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+    @NotBlank(message = "Message is required")
+    private String message;
 }
